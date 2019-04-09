99fa6b321c72e19bf61f7fee93b1d03887f72c89
YOUR NAME <YOUR@EMAIL.com>
2019-04-09 19:33:06 -0300

    aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,10 +1,13 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 config.paperclip_defaults = {
-:storage => :s3,
-:s3_region => 'us-east-2',
-:bucket => 'pictagram',
-:s3_credentials => "pict0gram/config/aws.yml",
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV['S3_BUCKET_NAME'],
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
+  }
 }
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -32,7 +35,7 @@ config.paperclip_defaults = {
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
