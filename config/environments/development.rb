1cf172904e9b4381ba9129f299400978c414b6d6
YOUR NAME <YOUR@EMAIL.com>
2019-04-09 16:13:08 -0300

    aws3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Paperclip.options[:command_path] = "/usr/local/bin/"
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
