34ed3cf98191a86748b2e50dd46cbd2d4d7930d2
YOUR NAME <YOUR@EMAIL.com>
2019-04-09 16:19:59 -0300

    aws5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,12 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+config.paperclip_defaults = {
+:storage => :s3,
+:s3_region => 'us-east-2',
+:bucket => 'pictagram',
+:path => '/:class/:attachment/:id_partition/:style/:filename',
+:s3_credentials => "#{Rails.root}/config/aws.yml",
+}
   # Code is not reloaded between requests.
   config.cache_classes = true
 
