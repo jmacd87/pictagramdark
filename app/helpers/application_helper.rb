1a0dbe12e3b95a40ec32614e8e17eac3e7e56374
YOUR NAME <YOUR@EMAIL.com>
2019-04-11 17:06:37 -0300

    aws

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,5 +21,12 @@ def profile_avatar_select(user)
   image_tag 'https://png.pngtree.com/svg/20161027/631929649c.svg', id: 'image-preview',
                                   class: 'img-responsive img-circle profile-image'
 end
-
+  helper_method :current_user
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
+  end
 end
