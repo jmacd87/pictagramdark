ba8ead5dbf154e5b72e0778443916f0d9a9d36c3
YOUR NAME <YOUR@EMAIL.com>
2019-04-11 17:11:34 -0300

    aws

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,7 +23,7 @@ def profile_avatar_select(user)
 end
   # helper_method :current_user
   def current_user
-    if session[:user_id]
+    if session[:user_id] != nil
       @current_user ||= User.find(session[:user_id])
     else
       @current_user = nil
