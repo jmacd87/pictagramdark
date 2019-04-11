f0523468db893ccbbe4684d774c9feb80b16c98b
YOUR NAME <YOUR@EMAIL.com>
2019-04-11 17:20:57 -0300

    aws

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
 	
-
+helper_method :current_user
+  def current_user
+    if session[:user_id] 
+      @current_user ||= User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
+  end
 
   def alert_for(flash_type)
   { success: 'alert-success',
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,12 +21,5 @@ def profile_avatar_select(user)
   image_tag 'https://png.pngtree.com/svg/20161027/631929649c.svg', id: 'image-preview',
                                   class: 'img-responsive img-circle profile-image'
 end
-  # helper_method :current_user
-  def current_user
-    if session[:user_id] != nil
-      @current_user ||= User.find(session[:user_id])
-    else
-      @current_user = nil
-    end
-  end
+  
 end
